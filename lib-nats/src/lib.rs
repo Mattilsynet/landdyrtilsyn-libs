30f8b669e12c71c79e1ef979a0d5310bbb01cd57
André Fosvold <andre.fosvold@mattilsynet.no>
2025-03-03 14:37:03 +0100

    chore fmt

--- lib-nats/src/lib.rs
+++ lib-nats/src/lib.rs
@@ -3,8 +3,8 @@ pub mod consumer;
 pub mod error;
 
 pub use async_nats::jetstream;
-pub use async_nats::Client;
 pub use async_nats::jetstream::consumer::PullConsumer;
 pub use async_nats::jetstream::AckKind;
 pub use async_nats::jetstream::{stream, Context};
+pub use async_nats::Client;
 pub use async_nats::HeaderMap;
