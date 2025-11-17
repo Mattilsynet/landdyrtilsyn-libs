c6e4696dd71e83bd36492999e105e44b01e1c2a9
Andreas Sigstad Lande <13892750+andreaslande@users.noreply.github.com>
2025-11-17 12:04:21 +0100

    Legger på _permissions i navngivning av lib

--- lib-entra/src/lib.rs
+++ lib-entra/src/lib.rs
@@ -1,6 +1,6 @@
-#[cfg(feature = "application_permission")]
-pub mod application;
-#[cfg(feature = "delegated_permission")]
-pub mod delegated;
+#[cfg(feature = "application_permissions")]
+pub mod application_permissions;
+#[cfg(feature = "delegated_permissions")]
+pub mod delegated_permissions;
 pub mod error;
 pub mod types;
