5ed58b79d10d8629e2879aaa85dc9b3379db8741
André Fosvold <andre.fosvold@mattilsynet.no>
2026-01-28 13:49:50 +0100

    skuffen: add CommandSequence type

--- lib-schemas/src/skuffen/command/commands.rs
+++ lib-schemas/src/skuffen/command/commands.rs
@@ -1,3 +1,4 @@
+use crate::error::{Result, SchemasError};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
@@ -25,4 +26,38 @@ pub struct CommandEnvelope<T> {
     pub command_id: Uuid,
     pub correlation_id: Option<Uuid>,
     pub payload: T,
+}
+
+pub struct CommandSequence(Vec<CommandEnvelope<Kommando>>);
+
+impl CommandSequence {
+    pub fn new(commands: Vec<CommandEnvelope<Kommando>>) -> Result<Self> {
+        if commands.is_empty() {
+            return Err(SchemasError::ValidationError(
+                "Command sequence must have at least 1 command".to_string(),
+            ));
+        }
+        Ok(Self(commands))
+    }
+
+    pub fn into_inner(self) -> Vec<CommandEnvelope<Kommando>> {
+        self.0
+    }
+}
+
+impl TryFrom<Vec<CommandEnvelope<Kommando>>> for CommandSequence {
+    type Error = SchemasError;
+
+    fn try_from(value: Vec<CommandEnvelope<Kommando>>) -> Result<Self> {
+        Self::new(value)
+    }
+}
+
+impl IntoIterator for CommandSequence {
+    type Item = CommandEnvelope<Kommando>;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
 }
